16467872545153a7ec93a876e4b7bb7317dda7fc
Glitch (glitch-hello-website) <none>
2025-02-18 12:24:09 +0000

    🍝🏜 Checkpoint ./Ejercicios/Ejercicio6/compgestor3.js:74616796/823 ./Ejercicios/Ejercicio6/compgestor2.js:74616796/1 ./Ejercicios/Ejercicio6/compgestor3.html:74616796/725 ./Ejercicios/Ejercicio6/compgestor4.js:74616796/856 ./Ejercicios/Ejercicio6/compgestor4.html:74616796/493

--- Ejercicios/Ejercicio6/compgestor4.js
+++ Ejercicios/Ejercicio6/compgestor4.js
@@ -1,14 +1,26 @@
 AFRAME.registerComponent('compgestor', {
                                      
     schema: {
+      elemento: {type: "selector", default: null},
       componente: {type: 'string', default: 'chivato'},
       tiempo: {type: 'int', default: 5000},
+      tiempo_antes: {type: 'int', default: 6000},
+      prop_nombre: {type: 'string', default: null},
+      prop_valor: {type: 'string', default: null},
     },
   
     init: function () {
+      
+      console.log('Init COMPGESTOR3');
+      
+      let ent = this.data.elemento;
+      if(!ent){
+        ent = this.el;
+      }
+      
+      ent.setAttribute(this.data.componente, {});
+      
       let self = this;
-      console.log('Init COMPGESTOR2');
-      this.el.setAttribute(this.data.componente, {});
       setTimeout (function (event) {
         console.log("QUITANDO");
         self.el.removeAttribute(self.data.componente);
