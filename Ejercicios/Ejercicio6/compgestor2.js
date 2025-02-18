3a70cc5d284c201679239259d867dacc2decd60d
Glitch (glitch-hello-website) <none>
2025-02-18 12:12:09 +0000

    🌱🖐 Checkpoint ./Ejercicios/Ejercicio6/compgestor2.js:74616796/452 ./Ejercicios/Ejercicio6/compgestor2.html:74616796/357

--- Ejercicios/Ejercicio6/compgestor2.js
+++ Ejercicios/Ejercicio6/compgestor2.js
@@ -2,32 +2,16 @@ AFRAME.registerComponent('compgestor', {
                                      
     schema: {
       componente: {type: 'string', default: 'chivato'},
-      componente: {type: 'int', default: 5000},
+      tiempo: {type: 'int', default: 5000},
     },
   
     init: function () {
+      let self = this;
       console.log('Init COMPGESTOR');
       this.el.setAttribute(this.data.componente, {});
       setTimeout (function (event) {
         console.log("QUITANDO");
-        this.el.removeAttribute(this.data.componente);
-      }, this.data
+        self.el.removeAttribute(self.data.componente);
+      }, this.data.tiempo);
     }
 });
-
-
-
-
-
-
-function anade(event) {
-          console.log("FLAG");
-          let ent = document.getElementById("entidad");
-          ent.setAttribute("chivato", {});
-          setTimeout(quitar, 5000);
-        }
-        function quitar (event) {
-          console.log("QUITANDO");
-          let ent = document.getElementById("entidad");
-          ent.removeAttribute("chivato");
-        }
