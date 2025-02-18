7454f1c9d7db1834163d48be2bd2e6957d175eb6
Glitch (glitch-hello-website) <none>
2025-02-18 10:41:12 +0000

    🕰🚓 Checkpoint ./Ejercicios/Ejercicio5/chivato_din2.html:74616796/1023 ./Ejercicios/Ejercicio5/chivato2_din2.html:74616796/1749 ./Ejercicios/Ejercicio5/chivato2.js:74616796/1844

---
+++ Ejercicios/Ejercicio5/chivato2.js
@@ -0,0 +1,20 @@
+AFRAME.registerComponent('chivato', {
+            init: function () {
+            console.log('Chivato ACTIVADO');
+              this.contador = 0;
+            },
+
+            tick: function () {
+              this.contador += 1;
+              if((this.contador % 10)==0){
+                console.log(this.contador);
+              }
+              if((this.contador % 100) == 0){
+                 console.log('Chivato 100 ACTIVADO');
+              }
+            },
+  
+            remove: function () {
+                console.log('Chivato quitado de la entidad');
+            }
+});
