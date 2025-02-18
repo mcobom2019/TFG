e5e6e2ea2d8232e2c2105095e40c5889f7281d4c
Glitch (glitch-hello-website) <none>
2025-02-18 12:01:05 +0000

    🌷😋 Checkpoint ./Ejercicios/Ejercicio6/compgestor2.js:74616796/554

--- Ejercicios/Ejercicio6/compgestor2.js
+++ Ejercicios/Ejercicio6/compgestor2.js
@@ -2,11 +2,32 @@ AFRAME.registerComponent('compgestor', {
                                      
     schema: {
       componente: {type: 'string', default: 'chivato'},
-      componente: {type: 'int', default: 5},
+      componente: {type: 'int', default: 5000},
     },
   
     init: function () {
       console.log('Init COMPGESTOR');
       this.el.setAttribute(this.data.componente, {});
-    },
+      setTimeout (function (event) {
+        console.log("QUITANDO");
+        this.el.removeAttribute(this.data.componente);
+      }, this.data
+    }
 });
+
+
+
+
+
+
+function anade(event) {
+          console.log("FLAG");
+          let ent = document.getElementById("entidad");
+          ent.setAttribute("chivato", {});
+          setTimeout(quitar, 5000);
+        }
+        function quitar (event) {
+          console.log("QUITANDO");
+          let ent = document.getElementById("entidad");
+          ent.removeAttribute("chivato");
+        }
