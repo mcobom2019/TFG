6c47f1460dae97b220b57aa717729c42429a4921
Glitch (glitch-hello-website) <none>
2025-02-19 11:34:54 +0000

    🐡🚏 Checkpoint ./Ejercicios/Ejercicio9/colorEhijos.js:74616796/2062 ./Ejercicios/Ejercicio9/colorEhijos.html:74616796/594 ./index.html:74616796/108

--- Ejercicios/Ejercicio9/colorEhijos.js
+++ Ejercicios/Ejercicio9/colorEhijos.js
@@ -6,52 +6,12 @@ AFRAME.registerComponent('createsons', {
         const scene = document.querySelector("a-scene");
         
         el.addEventListener('click', function() {
-            for(let i=0; )
-            const newBox = document.createElement("a-sphere");
-            newBox.setAttribute("color", "green");
-            newBox.setAttribute("position", `${Math.random() * 20 - 1} 1 -3`);
-            scene.appendChild(newBox);
-            
-            
-            setTimeout( function() {
-                const color = randomColor();
-                el.setAttribute('material', 'color', color);
-                var posicionactual = el.getAttribute('position');
-                var nuevaposicion = {
-                  x: posicionactual.x,
-                  y: posicionactual.y,
-                  z: posicionactual.z - 1,
-                };
-                el.setAttribute('position', nuevaposicion);
-            }, waitTime);
+            for(let i=0; i<3; i++){
+                var newSphere = document.createElement("a-sphere");
+                newSphere.setAttribute("color", "green");
+                newSphere.setAttribute("position", `0 ${(i*2)+2}-3`);
+                scene.appendChild(newSphere);
+            }
         });
       }
-  });
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    const btnBox = document.getElementById("btn-box");
-    const scene = document.querySelector("a-scene");
-
-    btnBox.addEventListener("click", () => {
-        const newBox = document.createElement("a-box");
-        newBox.setAttribute("color", "green");
-        newBox.setAttribute("position", `${Math.random() * 20 - 1} 1 -3`);
-        scene.appendChild(newBox);
-    });
 });
-
-
-
-
-
-function randomColor() {
-      const r = Math.floor(Math.random() * 256);
-      const g = Math.floor(Math.random() * 256);
-      const b = Math.floor(Math.random() * 256);
-            
-      const rgb = `rgb(${r},${g},${b})`;
-      console.log("El color es", rgb);
-            
-      return rgb;
-}
