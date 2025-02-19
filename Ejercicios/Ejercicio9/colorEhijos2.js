11705a9f2de91925eb898ae06d4e4da223a2638b
Glitch (glitch-hello-website) <none>
2025-02-19 17:55:34 +0000

    🐕🚴 Checkpoint ./Ejercicios/Ejercicio9/colorEhijos2.html:74616796/506 ./Ejercicios/Ejercicio9/colorEhijos2.js:74616796/4022

---
+++ Ejercicios/Ejercicio9/colorEhijos2.js
@@ -0,0 +1,46 @@
+AFRAME.registerComponent('createsons', {
+      init: function () {
+        console.log('Creando Hijos');
+        var el = this.el;
+        const btnSphere = document.getElementById("btn-sphere");
+        const scene = document.querySelector("a-scene");
+        
+        el.addEventListener('click', function() {
+            var parentPosition = el.getAttribute('position');
+            var parentY = parentPosition.y;
+            for(let i=0; i<3; i++){
+                var newSphere = document.createElement("a-sphere");
+                newSphere.setAttribute("color", "green");
+                newSphere.setAttribute("position", `${(i - 1)*0.5} ${parentY + 1} -3`);
+                newSphere.setAttribute("radius", "0.2");
+                newSphere.setAtributte("changecolor", "");
+                scene.appendChild(newSphere);
+            }
+        });
+      },
+});
+
+AFRAME.registerComponent('changecolor', {
+      init: function () {
+        console.log('Creando Hijos');
+        var el = this.el;
+        
+        el.addEventListener('click', function() {
+            var newColor = randomColor();
+            el.setAttribute('color', "0.2");
+                scene.appendChild(newSphere);
+            }
+        });
+      }
+});
+
+function randomColor() {
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+            
+      const rgb = `rgb(${r},${g},${b})`;
+      console.log("El color es", rgb);
+            
+      return rgb;
+}
