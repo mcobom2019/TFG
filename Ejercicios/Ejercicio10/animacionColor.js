9064870106f670fb54385e8577bb96dc7ad6a4ac
Glitch (glitch-hello-website) <none>
2025-02-20 11:07:34 +0000

    📠🌔 Checkpoint ./Ejercicios/Ejercicio10/animacionColor.js:74616796/349

--- Ejercicios/Ejercicio10/animacionColor.js
+++ Ejercicios/Ejercicio10/animacionColor.js
@@ -6,6 +6,15 @@ AFRAME.registerComponent('circular-animation', {
       
       init: function () {
         this.angle = 0;
+        var el = this.el;
+        var colors = {red, green, blue};
+        el.addEventListener('click', function() {
+           var waitTime = Math.random() * 3000;
+            setTimeout( function() {
+                var newColor = randomColor();
+                el.setAttribute('color', newColor);
+            }, waitTime);
+        });
       },
   
       tick: function (time, timeDelta){
@@ -15,6 +24,6 @@ AFRAME.registerComponent('circular-animation', {
         let x = this.data.radius * Math.cos(this.angle);
         let y = this.data.radius * Math.sin(this.angle);
 
-        this.el.setAttribute('position', { x: x, y: y, z: 0 });
+        this.el.setAttribute('position', { x: x, y: y, z: -4 });
       }
 });
