ec45b5e9153a65db57ef4cda1409aaf6a225b90b
Glitch (glitch-hello-website) <none>
2025-02-23 19:01:07 +0000

    Menú Flotante 1

---
+++ Menus/Menu1/README.md
@@ -0,0 +1,3 @@
+Menu:
+
+Si se haceclick sobre la esfera, nos aparecerá un menu flotante el cual tiene un boton de cerrar. Si se hace click sobre el boton cerrar(rojo) del menu, este se elimina. Si se vuelve a hacer click sobre la esfera, nos vuelve a crear un menú flotante.
--- Menus/Menu1/menu1.js
+++ Menus/Menu1/menu1.js
@@ -1,48 +1,45 @@
 AFRAME.registerComponent('createsons', {
     schema: {
-      width: { type: 'number', default: 1 },
-      height: { type: 'number', default: 1 },
-      depth: { type: 'number', default: 0.1 },
-      color: { type: 'string', default: '#333' }
+        width: { type: 'number', default: 1 },
+        height: { type: 'number', default: 1 },
+        depth: { type: 'number', default: 0.1 },
+        color: { type: 'string', default: '#333' }
     },
-  
+
     init: function () {
         var el = this.el;
-        const scene = document.querySelector("a-scene");
         var data = this.data;
-        var ok = false;
+        var scene = document.querySelector("a-scene");
+        var menuPanel = null; 
 
         el.addEventListener('click', function () {
-            if (!ok) {
+            if (!menuPanel) { 
                 var parentPosition = el.getAttribute('position');
-                var newPosition = `${parentPosition.x} ${parentPosition.y+2} ${parentPosition.z+2}`;
-              
-                const menuPanel = document.createElement('a-box');
+                var newPosition = `${parentPosition.x} ${parentPosition.y + 2} ${parentPosition.z + 2}`;
+
+                menuPanel = document.createElement('a-box');
                 menuPanel.setAttribute('width', data.width);
                 menuPanel.setAttribute('height', data.height);
                 menuPanel.setAttribute('depth', data.depth);
                 menuPanel.setAttribute('color', data.color);
-                menuPanel.setAttribute("position", newPosition);
-              
+                menuPanel.setAttribute('position', newPosition);
+
                 const closeButton = document.createElement('a-plane');
                 closeButton.setAttribute('width', '0.2');
                 closeButton.setAttribute('height', '0.2');
                 closeButton.setAttribute('color', 'red');
                 closeButton.setAttribute('position', '0.4 0.4 0.06');
+
                 menuPanel.appendChild(closeButton);
-                el.appendChild(menuPanel);
-                ok = true;
-              
+                scene.appendChild(menuPanel);
+
                 closeButton.addEventListener('click', function () {
-                    // Eliminar correctamente el menú
                     if (menuPanel && menuPanel.parentNode) {
                         menuPanel.parentNode.removeChild(menuPanel);
-                        menuPanel = null;
-                        ok = false;
+                        menuPanel = null; 
                     }
                 });
             }
-            
         });
     }
 });
