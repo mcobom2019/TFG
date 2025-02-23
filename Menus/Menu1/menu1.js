05c0fda07d46fb368ed0251f82d9fd93cc3a5038
Glitch (glitch-hello-website) <none>
2025-02-23 17:53:09 +0000

    💂👽 Checkpoint ./Menus/Menu1/menu1.js:74616796/3893

--- Menus/Menu1/menu1.js
+++ Menus/Menu1/menu1.js
@@ -1,40 +1,3 @@
-AFRAME.registerComponent('floating-menu', {
-
-  init: function () {
-    const el = this.el;
-    const data = this.data;
-
-    // Crear panel de menú
-    const menuPanel = document.createElement('a-box');
-    menuPanel.setAttribute('width', data.width);
-    menuPanel.setAttribute('height', data.height);
-    menuPanel.setAttribute('depth', data.depth);
-    menuPanel.setAttribute('color', data.color);
-    menuPanel.setAttribute('position', '0 1.5 -2');
-    el.appendChild(menuPanel);
-
-    // Crear botón de cerrar
-    const closeButton = document.createElement('a-plane');
-    closeButton.setAttribute('width', '0.2');
-    closeButton.setAttribute('height', '0.2');
-    closeButton.setAttribute('color', 'red');
-    closeButton.setAttribute('position', '0.4 0.4 0.06');
-    menuPanel.appendChild(closeButton);
-
-    closeButton.addEventListener('click', function () {
-      el.setAttribute('visible', false);
-    });
-
-    // Agregar evento para mostrar menú
-    el.sceneEl.addEventListener('menu-toggle', function () {
-      el.setAttribute('visible', true);
-    });
-  }
-});
-
-
-
-
 AFRAME.registerComponent('createsons', {
     schema: {
       width: { type: 'number', default: 1 },
@@ -52,17 +15,32 @@ AFRAME.registerComponent('createsons', {
         el.addEventListener('click', function () {
             if (!ok) {
                 var parentPosition = el.getAttribute('position');
-                var newMenu = document.createElement("floating-menu");
+                var newPosition = `${parentPosition.x} ${parentPosition.y+2} ${parentPosition.z+2}`;
               
-                newMenu.setAttribute('width', data.width);
-                newMenu.setAttribute('height', data.height);
-                newMenu.setAttribute('depth', data.depth);
-                newMenu.setAttribute('color', data.color);
-                var newPosition = `${parentPosition.x} ${parentPosition.y+2} ${parentPosition.z-1}`;
-                newMenu.setAttribute("position", newPosition);
-
-                scene.appendChild(newMenu);
+                const menuPanel = document.createElement('a-box');
+                menuPanel.setAttribute('width', data.width);
+                menuPanel.setAttribute('height', data.height);
+                menuPanel.setAttribute('depth', data.depth);
+                menuPanel.setAttribute('color', data.color);
+                menuPanel.setAttribute("position", newPosition);
+              
+                const closeButton = document.createElement('a-plane');
+                closeButton.setAttribute('width', '0.2');
+                closeButton.setAttribute('height', '0.2');
+                closeButton.setAttribute('color', 'red');
+                closeButton.setAttribute('position', '0.4 0.4 0.06');
+                menuPanel.appendChild(closeButton);
+                el.appendChild(menuPanel);
                 ok = true;
+              
+                closeButton.addEventListener('click', function () {
+                    // Eliminar correctamente el menú
+                    if (menuPanel && menuPanel.parentNode) {
+                        menuPanel.parentNode.removeChild(menuPanel);
+                        menuPanel = null;
+                        ok = false;
+                    }
+                });
             }
             
         });
