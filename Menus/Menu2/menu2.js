3a54125b7f73f36b3641a4a58d3b8699abe497c8
Glitch (glitch-hello-website) <none>
2025-02-25 09:52:12 +0000

    👓🏗 Checkpoint ./Menus/Menu2/menu2.js:74616796/23606 ./Menus/Menu2/menu2.html:74616796/607 ./index.html:74616796/110 ./Menus/Menu2/data.json:74616796/873

---
+++ Menus/Menu2/menu2.js
@@ -0,0 +1,136 @@
+AFRAME.registerComponent('createsons', {
+    init: function () {
+        var el = this.el;
+        var scene = document.querySelector("a-scene");
+        var menuPanel = null;
+        var barChartEntity = null;
+        var pieChartEntity = null;
+        var barsEntity = null;
+        var pieEntity = null;
+        var isBarChartVisible = false; // Estado del gráfico
+        var isPieChartVisible = false;
+
+        el.addEventListener('click', function () {
+            if (!menuPanel) {
+                var parentPosition = el.getAttribute('position');
+                var newPosition = { x: parentPosition.x, y: parentPosition.y + 1.5, z: parentPosition.z };
+
+                // Crear el menú flotante
+                menuPanel = document.createElement('a-box');
+                menuPanel.setAttribute('width', '1');
+                menuPanel.setAttribute('height', '0.7');
+                menuPanel.setAttribute('depth', '0.1');
+                menuPanel.setAttribute('color', '#333');
+                menuPanel.setAttribute('position', `${newPosition.x} ${newPosition.y} ${newPosition.z}`);
+
+                // Botón para mostrar/ocultar gráfico de barras
+                var barChartButton = document.createElement('a-plane');
+                barChartButton.setAttribute('width', '0.6');
+                barChartButton.setAttribute('height', '0.2');
+                barChartButton.setAttribute('color', 'blue');
+                barChartButton.setAttribute('position', '0 0 0.06');
+                barChartButton.setAttribute('text', 'value: Barras; color: white; align: center; width: 1.5;');
+                // Botón para mostrar/ocultar gráfico circular
+                var barChartButton2 = document.createElement('a-plane');
+                barChartButton2.setAttribute('width', '0.6');
+                barChartButton2.setAttribute('height', '0.2');
+                barChartButton2.setAttribute('color', 'blue');
+                barChartButton2.setAttribute('position', '0 -0.2 0.06');
+                barChartButton2.setAttribute('text', 'value: Circular; color: white; align: center; width: 1.5;');
+                
+
+                // Evento para mostrar/ocultar gráfico de barras
+                barChartButton.addEventListener('click', function () {
+                    if (!isBarChartVisible) {
+                        // Si el gráfico no está visible, crearlo
+                        barChartEntity = document.createElement('a-entity');
+                        barChartEntity.setAttribute('id', 'data');
+                        barChartEntity.setAttribute('babia-queryjson', 'url: ./data.json; path: data');
+
+                        barsEntity = document.createElement('a-entity');
+                        barsEntity.setAttribute('babia-barsmap', 'from: data; legend: true; palette: ubuntu; x_axis: model; z_axis: color; height: sales');
+                        barsEntity.setAttribute('position', `${newPosition.x+3} ${newPosition.y - 2} ${newPosition.z}`);
+                        barsEntity.setAttribute('scale', '0.2 0.2 0.2');
+
+                        scene.appendChild(barChartEntity);
+                        scene.appendChild(barsEntity);
+
+                        isBarChartVisible = true;
+                    } else {
+                        // Si el gráfico está visible, eliminarlo
+                        if (barChartEntity && barChartEntity.parentNode) {
+                            scene.removeChild(barChartEntity);
+                            scene.removeChild(barsEntity);
+                            barChartEntity = null;
+                            barsEntity = null;
+                        }
+                        isBarChartVisible = false;
+                    }
+                });
+              // Evento para mostrar/ocultar gráfico circular
+                barChartButton2.addEventListener('click', function () {
+                    if (!isBarChartVisible) {
+                        // Si el gráfico no está visible, crearlo
+                        pieChartEntity = document.createElement('a-entity');
+                        pieChartEntity.setAttribute('id', 'data2');
+                        pieChartEntity.setAttribute('babia-queryjson', 'url: ./data.json; path: data2');
+
+                        pieEntity = document.createElement('a-entity');
+                        pieEntity.setAttribute('babia-pie', 'from: data2; legend: true; palette: blues; key: model; size: doors;');
+                        pieEntity.setAttribute('position', `${newPosition.x+7} ${newPosition.y-1} ${newPosition.z}`);
+                        pieEntity.setAttribute('scale', '0.8 0.8 0.8');
+                        pieEntity.setAttribute('rotation', '90 0 0');
+                      
+
+                        scene.appendChild(pieChartEntity);
+                        scene.appendChild(pieEntity);
+
+                        isPieChartVisible = true;
+                    } else {
+                        // Si el gráfico está visible, eliminarlo
+                        if (pieChartEntity && pieChartEntity.parentNode) {
+                            scene.removeChild(pieChartEntity);
+                            scene.removeChild(pieEntity);
+                            pieChartEntity = null;
+                            pieEntity = null;
+                        }
+                        isPieChartVisible = false;
+                    }
+                });
+
+                // Botón para cerrar el menú y eliminar gráfico
+                var closeButton = document.createElement('a-plane');
+                closeButton.setAttribute('width', '0.2');
+                closeButton.setAttribute('height', '0.2');
+                closeButton.setAttribute('color', 'red');
+                closeButton.setAttribute('position', '0.4 0.3 0.06');
+                closeButton.setAttribute('text', 'value: X; color: white; align: center;');
+
+                closeButton.addEventListener('click', function () {
+                    if (menuPanel && menuPanel.parentNode) {
+                        menuPanel.parentNode.removeChild(menuPanel);
+                        menuPanel = null;
+                    }
+                    if (barChartEntity && barChartEntity.parentNode) {
+                        scene.removeChild(barChartEntity);
+                        scene.removeChild(barsEntity);
+                        barChartEntity = null;
+                        barsEntity = null;
+                        isBarChartVisible = false;
+                    }if (pieChartEntity && pieChartEntity.parentNode) {
+                        scene.removeChild(pieChartEntity);
+                        scene.removeChild(pieEntity);
+                        pieChartEntity = null;
+                        pieEntity = null;
+                        isPieChartVisible = false;
+                    }
+                });
+
+                menuPanel.appendChild(barChartButton);
+                menuPanel.appendChild(barChartButton2);
+                menuPanel.appendChild(closeButton);
+                scene.appendChild(menuPanel);
+            }
+        });
+    }
+});
