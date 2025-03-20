d3b9bd80a164265df59aea65c7685c156a141a8b
Glitch (proyecto-ejercicios-variados) <none>
2025-03-20 10:57:45 +0000

    🐸🛤 Checkpoint ./Manos/Manos4/manos4.1.html:74616796/986 ./Manos/Manos4/manos4.1.js:74616796/1235

--- Manos/Manos4/manos4.1.js
+++ Manos/Manos4/manos4.1.js
@@ -18,7 +18,14 @@ AFRAME.registerComponent('createsons', {
             }
         });
       
-
+        //evento para ver cuando se produce colision entre el dedo el boton rojo
+        el.addEventListener('obbcollisionstarted', function (event) {
+          if (event.detail && event.detail.target && event.detail.target.id === "indexFinger") {
+              if (!menuPanel) {
+                  crearMenuPrincipal();
+              }
+          }
+        });
 
         function crearMenuPrincipal() {
             cerrarMenus();
