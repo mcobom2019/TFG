43a2b7c00bde858d111c5b7b6bf40b61ee1fe865
Glitch (proyecto-ejercicios-variados) <none>
2025-03-20 17:00:44 +0000

    🗺🏋 Checkpoint ./Manos/Manos4/detector.js:74616796/5649 ./Manos/Manos4/manos4.1.html:74616796/7067 ./Manos/Manos4/mano-colision.js:74616796/6089

--- Manos/Manos4/detector.js
+++ Manos/Manos4/detector.js
@@ -1,30 +1,23 @@
 // Componente detector
 // Detecta cuando se pulsa el botón del ratón sobre un componente,
-// cuándo se produce una colisión, o cuándo se hace el gesto "pinch".
+// o cuando la mano interactúa con el componente.
 
 AFRAME.registerComponent("detector", {
     init: function () {
         let self = this;
         console.log("Detector: init");
+        
+        // Mantener eventos de click (para ratón y controladores)
         this.el.addEventListener("click", function () {
             console.log("Detector: Botón pulsado", self.el);
         });
-        this.el.addEventListener("obbcollisionstarted", function () {
-            console.log("Detector: Comienzo de colisión", self.el);
-        });
-        this.el.addEventListener("obbcollisionended", function () {
-            console.log("Detector: Fin de colisión", self.el);
-        });
-        this.el.addEventListener("pinchstarted", function () {
-            console.log("Detector: Comienzo de pellizco", self.el);
-        });
-        this.el.addEventListener("pinchended", function () {
-            console.log("Detector: Fin de pellizco", self.el);
-        });
+        
+        // Estos eventos se mantienen para compatibilidad
         this.el.addEventListener('controllerconnected', function (event) {
             console.log("Detector: Controlador conectado", self.el);
             console.log(event.detail.name);
         });
+        
         this.el.addEventListener('controllerdisconnected', function (event) {
             console.log("Detector: Controlador desconectado", self.el);
             console.log(event.detail.name);
