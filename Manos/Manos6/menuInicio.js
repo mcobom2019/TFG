4c601e26b18041bc44dedc9ce3063bb5d4b19144
Glitch (proyecto-ejercicios-variados) <none>
2025-03-25 10:12:17 +0000

    📼🍢 Checkpoint ./Manos/Manos6/menuInicio.js:74616796/1518 ./Manos/Manos6/submenu1.js:74616796/2143 ./Manos/Manos6/index.html:74616796/675 ./Manos/Manos6/pressable.js:74616796/2300 ./Manos/Manos6/button.js:74616796/4076

--- Manos/Manos6/menuInicio.js
+++ Manos/Manos6/menuInicio.js
@@ -22,10 +22,17 @@ AFRAME.registerComponent('controlinicio', {
 
   init: function () {
     this.bindMethods();
-
+  
+    //boton start
     this.startButtonEl = document.querySelector('#startButton');
     this.startButtonEl.addEventListener('click', this.onClick);
     this.startButtonEl.addEventListener('pressedended', this.onClick);
+    
+    //boton X
+    this.xButtonEl = document.querySelector('#xButton');
+    this.xButtonEl.addEventListener('click', this.onClick);
+    this.xButtonEl.addEventListener('pressedended', this.onClick);
+    
   },
 
   bindMethods: function () {
@@ -34,30 +41,14 @@ AFRAME.registerComponent('controlinicio', {
 
   onClick: function (evt) {
     var targetEl = evt.target;
-    if (targetEl === this.boxButtonEl ||
-        targetEl === this.sphereButtonEl ||
-        targetEl === this.torusButtonEl) {
-      this.boxButtonEl.removeState('pressed');
-      this.sphereButtonEl.removeState('pressed');
-      this.torusButtonEl.removeState('pressed');
-      this.boxGeometryEl.object3D.visible = false;
-      this.sphereGeometryEl.object3D.visible = false;
-      this.torusGeometryEl.object3D.visible = false;
-      this.buttonToGeometry[targetEl.id].object3D.visible = true;
+    if (targetEl === this.startButtonEl || targetEl.xButtonEl) {
+      this.submenu1.visible = true;
+      this.menuInicio.visible = false;
+    }
+    
+    if (targetEl === this.startButtonEl || targetEl.xButtonEl) {
+      this.menuInicio.visible = false;
     }
 
-    if (targetEl === this.darkModeButtonEl) {
-      if (this.el.sceneEl.is('starry')) {
-        targetEl.setAttribute('button', 'label', 'Dark Mode');
-        this.el.sceneEl.setAttribute('environment', {preset: 'default'});
-        this.el.sceneEl.removeState('starry');
-      } else {
-        targetEl.setAttribute('button', 'label', 'Light Mode');
-        this.el.sceneEl.setAttribute('environment', {preset: 'starry'});
-        this.el.sceneEl.addState('starry');
-      }
-    } else {
-      targetEl.addState('pressed');
-    }
   }
 });
---
+++ Manos/Manos6/submenu1.js
@@ -0,0 +1,63 @@
+/* global AFRAME */
+AFRAME.registerComponent('subMenu1', {
+  init: function () {
+    var el = this.el;
+    var menuBackGroundEl = document.createElement('a-entity');
+    menuBackGroundEl.setAttribute('geometry', {
+      primitive: 'box',
+      width: 0.6,
+      height: 0.40,
+      depth: 0.01
+    });
+    menuBackGroundEl.setAttribute('material', {
+      color: 'gray'
+    });
+    menuBackGroundEl.setAttribute('position', '0 0 -0.025');
+    el.appendChild(menuBackGroundEl);
+  }
+});
+
+/* global AFRAME */
+AFRAME.registerComponent('controsubmenu1', {
+
+  init: function () {
+    this.bindMethods();
+  
+    //boton barras
+    this.barrasButtonEl = document.querySelector('#barrasButton');
+    this.barrasButtonEl.addEventListener('click', this.onClick);
+    this.barrasButtonEl.addEventListener('pressedended', this.onClick);
+    
+    //boton circular
+    this.circularButtonEl = document.querySelector('#circularButton');
+    this.circularButtonEl.addEventListener('click', this.onClick);
+    this.circularButtonEl.addEventListener('pressedended', this.onClick);
+    
+    //boton atras
+    this.circularButtonEl = document.querySelector('#circularButton');
+    this.circularButtonEl.addEventListener('click', this.onClick);
+    this.circularButtonEl.addEventListener('pressedended', this.onClick);
+    
+  },
+
+  bindMethods: function () {
+    this.onClick = this.onClick.bind(this);
+  },
+
+  onClick: function (evt) {
+    var targetEl = evt.target;
+    if (targetEl === this.barrasButtonEl) {
+      this.submenu1.visible = false;
+      this.menuInicio.visible = false;
+    }
+    if (targetEl === this.circularButtonEl) {
+      this.submenu1.visible = false;
+      this.menuInicio.visible = false;
+    }
+    
+    if (targetEl === this.startButtonEl || targetEl.xButtonEl) {
+      this.menuInicio.visible = false;
+    }
+
+  }
+});
