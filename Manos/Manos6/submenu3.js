8178a1b4f2c2551da518c745ee195de2ea1df83e
Glitch (proyecto-ejercicios-variados) <none>
2025-03-25 11:20:31 +0000

    ⛴🏋 Checkpoint ./Manos/Manos6/index.html:74616796/1006 ./Manos/Manos6/submenu3.js:74616796/2769

---
+++ Manos/Manos6/submenu3.js
@@ -0,0 +1,89 @@
+/* global AFRAME */
+AFRAME.registerComponent('submenu2', {
+  init: function () {
+    var el = this.el;
+    var menuBackGroundEl = document.createElement('a-entity');
+    menuBackGroundEl.setAttribute('geometry', {
+      primitive: 'box',
+      width: 0.6,
+      height: 0.40,
+      depth: 0.01
+    });
+    menuBackGroundEl.setAttribute('material', {
+      color: 'gray'
+    });
+    menuBackGroundEl.setAttribute('position', '0 0 -0.025');
+    el.appendChild(menuBackGroundEl);
+  }
+});
+
+/* global AFRAME */
+AFRAME.registerComponent('controsubmenu2', {
+
+  init: function () {
+    this.bindMethods();
+    
+    this.menuInicio = document.querySelector('#menuinicio');
+    this.submenu1 = document.querySelector('#subMenu1');
+    this.submenu2 = document.querySelector('#subMenu2');
+  
+    //boton atras
+    this.backButtonEl = document.querySelector('#backButton');
+    this.backButtonEl.addEventListener('click', this.onClick);
+    
+    //boton Motor
+    this.motorButtonEl = document.querySelector('#motorButton');
+    this.motorButtonEl.addEventListener('click', this.onClick);
+    
+    //boton Color
+    this.colorButtonEl = document.querySelector('#colorButton');
+    this.colorButtonEl.addEventListener('click', this.onClick);
+    
+    //boton Puertas
+    this.puertasButtonEl = document.querySelector('#puertasButton');
+    this.puertasButtonEl.addEventListener('click', this.onClick);
+    
+    //boton Completo
+    this.completoButtonEl = document.querySelector('#completoButton');
+    this.completoButtonEl.addEventListener('click', this.onClick);
+    
+  },
+
+  bindMethods: function () {
+    this.onClick = this.onClick.bind(this);
+  },
+
+  onClick: function (evt) {
+    var targetEl = evt.target;
+    if (targetEl === this.backButtonEl) {
+      this.submenu1.setAttribute('visible', true);
+      this.submenu2.setAttribute('visible', false);
+      this.menuInicio.setAttribute('visible', false);
+    }
+    
+    if (targetEl === this.motorButtonEl) {
+      this.submenu1.setAttribute('visible', false);
+      this.submenu2.setAttribute('visible', false);
+      this.menuInicio.setAttribute('visible', false);
+    }
+    
+    if (targetEl === this.colorButtonEl) {
+      this.submenu1.setAttribute('visible', false);
+      this.submenu2.setAttribute('visible', false);
+      this.menuInicio.setAttribute('visible', false);
+    }
+    
+    if (targetEl === this.puertasButtonEl) {
+      this.submenu1.setAttribute('visible', false);
+      this.submenu2.setAttribute('visible', false);
+      this.menuInicio.setAttribute('visible', false);
+    }
+    
+    if (targetEl === this.completoButtonEl) {
+      this.submenu1.setAttribute('visible', false);
+      this.submenu2.setAttribute('visible', false);
+      this.menuInicio.setAttribute('visible', false);
+    }
+
+  }
+});
