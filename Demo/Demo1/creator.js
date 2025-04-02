b498559611e9a04b84c8bb17fc5dacf0f379a151
Glitch (proyecto-ejercicios-variados) <none>
2025-04-02 08:34:42 +0000

    📌👽 Checkpoint ./Demo/Demo1/initMenu.js:74616796/36 ./Demo/Demo1/creator.js:74616796/9682 ./Demo/Demo1/index.html:74616796/71 ./index.html:74616796/137

--- Demo/Demo1/creator.js
+++ Demo/Demo1/creator.js
@@ -19,6 +19,7 @@ AFRAME.registerComponent('loader', {
         
         if(pMenu.id){
           menuEl.setAttribute('id', pMenu.id);
+           menuEl.setAttribute('menuinicio', '');
         }
         if(pMenu.position){
           menuEl.setAttribute('position', pMenu.position);
@@ -29,94 +30,8 @@ AFRAME.registerComponent('loader', {
         if(pMenu.grabbable){
           menuEl.setAttribute('grababble', '');
         }
-        //});
-        data.scene.entities.forEach(entity => {
-          // Crear el elemento según su tipo
-          const entityEl = document.createElement('a-' + entity.type);
-          
-          // Asignar el ID si existe
-          if (entity.id) {
-            entityEl.setAttribute('id', entity.id);
-          }
-          
-          // Configurar posición
-          if (entity.position) {
-            entityEl.setAttribute('position', {
-              x: entity.position.x,
-              y: entity.position.y,
-              z: entity.position.z
-            });
-          }
-          
-          // Configurar rotación
-          if (entity.rotation) {
-            entityEl.setAttribute('rotation', {
-              x: entity.rotation.x,
-              y: entity.rotation.y,
-              z: entity.rotation.z
-            });
-          }
-          
-          // Configurar color
-          if (entity.color) {
-            entityEl.setAttribute('color', entity.color);
-          }
-          
-          // Configurar atributos específicos según el tipo
-          switch (entity.type) {
-            case 'plane':
-              if (entity.width) entityEl.setAttribute('width', entity.width);
-              if (entity.height) entityEl.setAttribute('height', entity.height);
-              break;
-            case 'box':
-              if (entity.width) entityEl.setAttribute('width', entity.width);
-              if (entity.height) entityEl.setAttribute('height', entity.height);
-              if (entity.depth) entityEl.setAttribute('depth', entity.depth);
-              break;
-            case 'sphere':
-              if (entity.radius) entityEl.setAttribute('radius', entity.radius);
-              break;
-            case 'cylinder':
-              if (entity.radius) entityEl.setAttribute('radius', entity.radius);
-              if (entity.height) entityEl.setAttribute('height', entity.height);
-              break;
-            case 'text':
-              if (entity.value) entityEl.setAttribute('value', entity.value);
-              if (entity.width) entityEl.setAttribute('width', entity.width);
-              if (entity.align) entityEl.setAttribute('align', entity.align);
-              break;
-          }
-          
-          // Añadir entidad a la escena
-          sceneEl.appendChild(entityEl);
-        });
-        
-        // Crear luces
-        data.scene.lights.forEach(light => {
-          const lightEl = document.createElement('a-light');
-          
-          // Configurar tipo de luz
-          lightEl.setAttribute('type', light.type);
-          
-          // Configurar color e intensidad
-          lightEl.setAttribute('color', light.color);
-          lightEl.setAttribute('intensity', light.intensity);
-          
-          // Configurar posición
-          if (light.position) {
-            lightEl.setAttribute('position', {
-              x: light.position.x,
-              y: light.position.y,
-              z: light.position.z
-            });
-          }
-          
-          // Añadir luz a la escena
-          sceneEl.appendChild(lightEl);
-        });
+      
+        sceneEl.appendChild(menuEl);
       })
-      .catch(error => {
-        console.error('Error cargando la escena desde JSON:', error);
-      });
   }
 });
