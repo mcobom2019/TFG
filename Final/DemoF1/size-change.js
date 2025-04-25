3926f3716093ec313abbbc0c5c33b717b493a0ee
Glitch (proyecto-ejercicios-variados) <none>
2025-04-25 10:37:36 +0000

    🚩🚵 Checkpoint ./Final/DemoF1/scene.json:74616796/29 ./Final/DemoF1/size-change.js:74616796/2026 ./Final/DemoF1/menu.js:74616796/31

--- Final/DemoF1/size-change.js
+++ Final/DemoF1/size-change.js
@@ -10,7 +10,24 @@ AFRAME.registerComponent('size-change', {
   },
 
   onSliderChanged: function (evt) {
-    var scale = evt.detail.value;
-    this.el.object3D.scale.set(scale, scale, scale);
+    var sliderValue = evt.detail.value;
+
+    // Definir rangos de escala diferentes para cada eje
+    var minScaleX = 0.1;  // Escala mínima para X
+    var maxScaleX = 0.8;  // Escala máxima para X (más restrictiva)
+
+    var minScaleY = 0.1;  // Escala mínima para Y
+    var maxScaleY = 1.5;  // Escala máxima para Y (puede ser más alta)
+
+    var minScaleZ = 0.1;  // Escala mínima para Z
+    var maxScaleZ = 1.0;  // Escala máxima para Z
+
+    // Calcular las escalas finales basadas en el valor del slider
+    var scaleX = minScaleX + sliderValue * (maxScaleX - minScaleX);
+    var scaleY = minScaleY + sliderValue * (maxScaleY - minScaleY);
+    var scaleZ = minScaleZ + sliderValue * (maxScaleZ - minScaleZ);
+
+    // Aplicar las escalas diferentes a cada eje
+    this.el.object3D.scale.set(scaleX, scaleY, scaleZ);
   }
 });
