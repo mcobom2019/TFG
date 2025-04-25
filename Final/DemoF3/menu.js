858b5cd3d8251ba109c30a10560f9ee6d0fc71cd
Glitch (proyecto-ejercicios-variados) <none>
2025-04-25 18:35:54 +0000

    📷🌦 Checkpoint ./.glitch-assets:74616796/412 ./Final/DemoF3/scene.json:74616796/45 ./Final/DemoF3/menu.js:74616796/463

--- Final/DemoF3/menu.js
+++ Final/DemoF3/menu.js
@@ -231,6 +231,8 @@ AFRAME.registerComponent('menu', {
         audioEl.id = 'backgroundMusic';
         if(name == "birds"){
           audioEl.src = 'https://cdn.glitch.global/1f8e0b5c-8472-495a-a6ce-b620a6cdfd40/cantos-de-pajaros-para-relajar_btET4k1W.mp3?v=1745605337904';
+        }else if(name == "water"){
+          audioEl.src = "https://cdn.glitch.global/1f8e0b5c-8472-495a-a6ce-b620a6cdfd40/agua-de-rio-fluyendo_elp1GLMm.mp3?v=1745605564985";
         }else if(name == "gladiator"){
           audioEl.src = 'https://cdn.glitch.global/1f8e0b5c-8472-495a-a6ce-b620a6cdfd40/now-we-are-free_eTmCadBH.mp3?v=1745604914553';
         }else if(name == "Darling Pretty"){
